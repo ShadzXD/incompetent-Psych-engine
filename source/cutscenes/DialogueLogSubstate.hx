ebf29fde477136227e9e7a2fd1b9682f72128c54
ShadzXD <136199318+ShadzXD@users.noreply.github.com>
2025-12-08 17:05:36 +0100

    Shittily finalise the dialogue record stuff

--- source/cutscenes/DialogueLogSubstate.hx
+++ source/cutscenes/DialogueLogSubstate.hx
@@ -7,20 +7,21 @@ class DialogueLogSubstate extends MusicBeatSubstate
 {
     var dialogue_lines:Array<String> = [""];
     var character_names_string:Array<String> = [""];
-    var icon_names:Array<String> = [""];
+    var dialogueGroup:FlxTypedGroup<FlxText>;
+    final TEXT_MOVEMENT_SPEED:Int = 4;
 
     public function new(curDialogue:Array<DialogueLine>)
     {
         super();
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
 
+
+
         for (i in 0...curDialogue.length)
         {
             trace(curDialogue[i].text);
             dialogue_lines[i] = curDialogue[i].text;
             character_names_string[i] = curDialogue[i].portrait;
-             icon_names[i] = curDialogue[i].iconName;
-
         }
     }
     override function create()
@@ -36,35 +37,39 @@ class DialogueLogSubstate extends MusicBeatSubstate
         var title:FlxText = new FlxText(0, 0, FlxG.width, "DIALOGUE RECORD", 23);
         title.setFormat(Paths.font('vcr.ttf'), 32, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
         add(title);
-
+        
+	    dialogueGroup = new FlxTypedGroup<FlxText>();
+		add(dialogueGroup);
         for (i in 0... dialogue_lines.length)
         {
             var names:FlxText = new FlxText(20, 40 + (i * 80), FlxG.width, character_names_string[i], 25);
             names.setFormat(Paths.font('vcr.ttf'), 25, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-            add(names);
-
-            var icon:HealthIcon = new HealthIcon('icon-bf', false);
-            icon.x = names.x + 80;
-            icon.y = names.y - 30;
-            icon.scale.set(0.4, 0.4);
-            add(icon);
+            dialogueGroup.add(names);
 
             var dialogue:FlxText = new FlxText(20, 70 + (i * 80), FlxG.width, dialogue_lines[i], 32);
             dialogue.setFormat(Paths.font('vcr.ttf'), 32, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-            add(dialogue);
+            dialogueGroup.add(dialogue);
         }
     }
-
     override  function update(elapsed:Float)
     {
-        super.update(elapsed / 2); //saves on processes time since this is only gonna be used for if checks
+        super.update(elapsed / 2);
         if(controls.BACK) close();
-    }
+        if(dialogueGroup.length > 7)
+        {
+            if (controls.UI_UP)
+            for (item in dialogueGroup)
+			    item.y -= TEXT_MOVEMENT_SPEED;
 
+	        if (controls.UI_DOWN)
+                for (item in dialogueGroup)
+			    item.y += TEXT_MOVEMENT_SPEED;
+        }
+      
+    }
     
 	override function destroy()
 	{
-        trace('destroy vars');
 		super.destroy();
 	}
 }
